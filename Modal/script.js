4ea1b032bc9a01b0c9a70b133d15c09fa1528045
Chairat <mr.chairat_lucky@icloud.com>
2021-12-26 16:52:26 +0700

    document.querySelectorAll

--- Modal/script.js
+++ Modal/script.js
@@ -1,3 +1,35 @@
 'use strict';
 
 //selection variable
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+//3 btn show modal
+const btnOpenModalList = document.querySelectorAll('.show-modal');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+btnOpenModalList.forEach(member => {
+  member.addEventListener('click', openModal);
+});
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//Keyboard
+document.addEventListener('keydown', e => {
+  console.log(e.key);
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
